df76a779ec7f58b4efdf4b34f1968d04aeca95cb
BrunoHunoff <brunohunoff@gmail.com>
2024-09-17 20:36:05 -0300

    adiciona dependencias

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
