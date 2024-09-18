81d2b7885d5e58b629ed6e548727f0bfb838842e
BrunoHunoff <brunohunoff@gmail.com>
2024-09-18 17:47:14 -0300

    ajuste na classe program

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var app = builder.Build();
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
-
 
 app.UseSwagger();
 app.UseSwaggerUI();
