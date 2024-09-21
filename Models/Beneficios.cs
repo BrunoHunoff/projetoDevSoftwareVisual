9a2bc202969aea5e8bec06292e269d4e84b690b3
Nicolas <nicolasrapp05@gmail.com>
2024-09-21 09:28:33 -0300

    Criação da model 'Beneficios'

---
+++ Models/Beneficios.cs
@@ -0,0 +1,7 @@
+public class Beneficios
+{
+    public int Id { get; set; }
+    public string NomeBeneficio { get; set; }
+    public string Descricao { get; set; }
+    public decimal Valor { get; set; }
+}
